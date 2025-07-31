9719f09f340c988d60722e517937618b6b1ff12d
Saujas04 <Saujas06@gmail.com>
2025-07-31 16:10:04 +0530

    Update qrMap.js

--- utils/qrMap.js
+++ utils/qrMap.js
@@ -1,12 +1,40 @@
 // Define correct QR IDs per team and clue number
 module.exports = {
   team1: {
-    1: "qr_t1_c1",
-    2: "qr_t1_c2"
+    1: "qr138",
+    2: "qr176",
+    3: "qr213",
+    4: "qr152",
+    5: "qr112"
   },
   team2: {
-    1: "qr_t2_c1",
-    2: "qr_t2_c2"
+    1: "qr139",
+    2: "qr176",
+    3: "qr214",
+    4: "qr399",
+    5: "qr272"
   },
+  team3: {
+    1: "qr140",
+    2: "qr176",
+    3: "qr215",
+    4: "qr152",
+    5: "qr112"
+  },
+  team4: {
+    1: "qr141",
+    2: "qr176",
+    3: "qr216",
+    4: "qr399",
+    5: "qr112"
+  },
+  team5: {
+    1: "qr142",
+    2: "qr176",
+    3: "qr217",
+    4: "qr399",
+    5: "qr272"
+  },
+  
   // Add more teams and clues as needed
 };
